2c46ae8771bf27eb95eaf3a14111c6ba46a5feef
foster-adu <adufoster1@yahoo.com>
2023-12-06 16:24:14 -0800

    python import modules

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
-if __name__ == "__main__":
-"""Print the value of variable a from variable_load_5."""
 from variable_load_5 import a
 
-print(a)
+
+def find_var():
+    print(a)
+
+
+if __name__ == "__main__":
+    find_var()
